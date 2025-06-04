27e6353b84e39839af8e356e9ea11681ac5444ed
Qondzio <qondziolabaziewicz@gmail.com>
2025-06-04 17:50:13 +0200

    Deleted pre-added projects and tasks. Also added a function to make a 'hamburger' work and display 'main-left'.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles.css"
+
+const hamburger=document.getElementById("hamburger_menu");
+
+hamburger.addEventListener('click', ()=>{
+    const leftDiv=document.querySelector(".main_left");
+    leftDiv.classList.toggle("main_left_active");
+})
